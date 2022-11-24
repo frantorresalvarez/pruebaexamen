18aed14c071b66ddb025311ee9a2aeaf0749befc
frantorresalvarez <franciscotorresdj@gmail.com>
2022-11-24 10:38:23 +0100

    Subo mi calculadora

---
+++ calculatrueno/src/calculatrueno/Calculatrueno.java
@@ -0,0 +1,17 @@
+
+package calculatrueno;
+
+
+public class Calculatrueno {
+
+  
+    public static void main(String[] args) {
+    
+    tardaensegundos = 5
+    velocidaddelsonido = 331 
+    distancia = tardaensegundos*velocidaddelsonido
+    System.out.println("que sepas que la tormenta esta a:"+str (distancia));
+            
+    }
+    
+}
